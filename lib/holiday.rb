43913b61186dc408a7d845b5a742c42f3c20ef2c
Isa Levine <isaacdlevine@gmail.com>
2019-01-21 07:42:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_values.each do |holiday_keys, supply_values|
       holidayname = holiday_keys.split("_").map(&capitalize).join
       items = supply_values.join(", ")
-      puts "  #{holiday_keys.capitalize}: " + items
+      puts holidayname + ": " + items
     end
   end
 end
